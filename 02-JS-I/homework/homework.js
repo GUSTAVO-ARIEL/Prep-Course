d52729d539e69b9f32f91506b2c49f4d5b763c25
GUSTAVO-ARIEL <martinezgustavoa4@gmail.com>
2022-11-15 17:52:06 -0300

    sept commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -98,22 +98,30 @@ function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
   return x % y
-  
+
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 === 0){
+
+    return true 
+  } 
+  else { false}
+
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if ( num % 2 === 1)
+  return true
 }
+
+
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
